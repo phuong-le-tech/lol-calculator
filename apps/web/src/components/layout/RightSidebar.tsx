16943523073fdb0f23aea3b2cefae543eb268ff8
Phuong LE <phuong@cfpgroup.fr>
2026-03-19 19:36:23 +0100

    feat: add right sidebar with dummy target panel and combat results

--- apps/web/src/components/layout/RightSidebar.tsx
+++ apps/web/src/components/layout/RightSidebar.tsx
@@ -1,7 +1,9 @@
+import { TargetPanel } from "../target/TargetPanel";
+
 export function RightSidebar() {
   return (
     <aside className="flex w-[320px] shrink-0 flex-col gap-3 overflow-y-auto border-l border-dark-200 bg-dark-500 p-4">
-      <p className="text-dark-50">Right sidebar</p>
+      <TargetPanel />
     </aside>
   );
 }
